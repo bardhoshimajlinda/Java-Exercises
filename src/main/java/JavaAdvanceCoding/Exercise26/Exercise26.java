56e3692da2a1634c24e92203845443fb9f17a334
bardhoshimajlinda <116483572+bardhoshimajlinda@users.noreply.github.com>
2023-12-03 01:45:58 +0100

    Java advance coding exercise 16

--- src/main/java/JavaAdvanceCoding/Exercise26/Exercise26.java
+++ src/main/java/JavaAdvanceCoding/Exercise26/Exercise26.java
@@ -1,7 +1,7 @@
 package JavaAdvanceCoding.Exercise26;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /* Using functional programming mechanisms based on the given structure, display:
 1. a list of all Models,
@@ -23,29 +23,141 @@ manufacturer,
  */
 public class Exercise26 {
     public static void main(String[] args) {
-        Car car1 = new Car("Audi A3", "Compact luxury car", CarType.HATCHBACK);
-        Car car2 = new Car("BMW 3 Series", "Compact executive car", CarType.SEDAN);
-        Car car3 = new Car("Porsche 911", "Sports car", CarType.COUPE);
+        Car car1 = new Car("Car1", "Description1", CarType.COUPE);
+        Car car2 = new Car("Car2", "Description2", CarType.CABRIO);
+        Car car3 = new Car("Car3", "Description3", CarType.SEDAN);
+        Model model1 = new Model("Model1", 2018, Arrays.asList(car1, car2));
+        Model model2 = new Model("Model2", 2020, Arrays.asList(car3));
+        Manufacturer manufacturer1 = new Manufacturer("Manufacturer1", 1910, Arrays.asList(model1, model2));
+        Manufacturer manufacturer2 = new Manufacturer("Manufacturer2", 1920, Arrays.asList(model1, model2));
 
-        List<Car> cars1 = new ArrayList<>();
-        cars1.add(car1);
-        cars1.add(car2);
+        List<Manufacturer> manufacturers = Arrays.asList(manufacturer1, manufacturer2);
 
-        List<Car> cars2 = new ArrayList<>();
-        cars2.add(car3);
 
-        Model audiModel = new Model("Audi A3", 2010, cars1);
-        Model bmwModel = new Model("BMW 3 Series", 2005, cars2);
+        //1. a list of all Models
+        List<Model> models = manufacturers.stream()
+                .flatMap(manufacturer -> manufacturer.models.stream())
+                .toList();
+        System.out.println("All models " + models);
 
-        List<Model> audiModels = new ArrayList<>();
-        audiModels.add(audiModel);
-        List<Model> bmwModels = new ArrayList<>();
-        bmwModels.add(bmwModel);
+        //2. a list of all cars
+//        List<Car> cars = models.stream()
+//                .flatMap(model -> model.cars.stream())
+//                .collect(Collectors.toList());
+//        System.out.println("All cars "+ cars);
 
-        Manufacturer bmwManufacturer = new Manufacturer("BMW", 1916, bmwModels);
+        // or like this:
 
-        audiModels.stream().forEach(System.out::println);
+        List<Car> cars = manufacturers.stream()
+                .flatMap(manufacturer -> manufacturer.models.stream())
+                .flatMap(model -> model.cars.stream())
+                .collect(Collectors.toList());
+        System.out.println(cars);
 
+
+        //3. list of all manufacturer names
+        List<String> manufacturerNames = manufacturers.stream()
+                .map(manufacturer -> manufacturer.name)
+                .toList();
+        System.out.println("Manufacturer names: " + manufacturerNames);
+
+
+        //4. list of all manufacturers' establishment years
+        List<Integer> establishmentYears = manufacturers.stream()
+                .map(manufacturer -> manufacturer.yearOfCreation)
+                .collect(Collectors.toList());
+        System.out.println("Establishment Years: " + establishmentYears);
+
+
+        //5. list of all model names
+        List<String> modelNames = manufacturers.stream()
+                .flatMap(manufacturer -> manufacturer.models.stream())
+                .map(model -> model.name)
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println("Model names " + modelNames );
+
+
+        //6. list of all years of starting production of models
+        List<Integer> productionStartingYears = models.stream()
+                .map(model -> model.productionStartYear)
+                .collect(Collectors.toList());
+        System.out.println("All years of starting production of models " + productionStartingYears);
+
+
+        //7. list of all car names
+        List<String> carNames = models.stream()
+                .flatMap(car -> car.cars.stream())
+                .map(car -> car.name)
+                .collect(Collectors.toList());
+        System.out.println("Car names: " + carNames);
+
+
+        //8. list of all car descriptions
+        List<String> carDescription = cars.stream()
+                .map(car -> car.description)
+                .collect(Collectors.toList());
+        System.out.println("Car descriptions " + carDescription);
+
+        //9. only models with an even year of production start
+        List<Model> modelsWithEvenYearOfProduction = models.stream()
+                .filter(model -> model.productionStartYear %2 == 0)
+                .collect(Collectors.toList());
+        System.out.println("Models with an even year of production start " + modelsWithEvenYearOfProduction);
+
+        //10. only cars from manufacturers with an even year of foundation
+        List<Car> carsFromEvenYearManufacturers = manufacturers.stream()
+                .filter(manufacturer -> manufacturer.yearOfCreation %2 == 0)
+                .flatMap(manufacturer -> manufacturer.models.stream())
+                .flatMap(model -> model.cars.stream())
+                .toList();
+        System.out.println("Cars from manufacturers with an even year of foundation: " + carsFromEvenYearManufacturers);
+
+
+        //11. only cars with an even year of starting production of the model and an odd year of establishing the manufacturer
+        List<Car> evenYearOfStartingProductionAndOddYearOfEstablishing = manufacturers.stream()
+                .filter(manufacturer -> manufacturer.yearOfCreation %2 != 0)
+                .flatMap(manufacturer -> manufacturer.models.stream())
+                .filter(model -> model.productionStartYear %2 == 0)
+                .flatMap(model -> model.cars.stream())
+                .collect(Collectors.toList());
+
+        if (evenYearOfStartingProductionAndOddYearOfEstablishing.isEmpty()) {
+            System.out.println("No cars found with the specified conditions.");
+        } else {
+            System.out.println("Cars with an even year of starting production of the model and an odd year of establishing the manufacturer " + evenYearOfStartingProductionAndOddYearOfEstablishing);
+        }
+
+
+        //12. only CABRIO cars with an odd year of starting model production and an even year of establishing the manufacturer
+        List<Car> cabrioCars = manufacturers.stream()
+                .filter(manufacturer -> manufacturer.yearOfCreation % 2 == 0)
+                .flatMap(manufacturer -> manufacturer.models.stream())
+                .filter(model -> model.productionStartYear % 2 != 0)
+                .flatMap(model -> model.cars.stream())
+                .filter(car -> car.carType.equals(CarType.CABRIO))
+                .collect(Collectors.toList());
+
+        if (cabrioCars.isEmpty()) {
+            System.out.println("No cars found with the specified conditions.");
+        } else {
+            System.out.println("CABRIO cars with an odd year of starting model production and an even year of establishing the manufacturer " + cabrioCars);
+        }
+
+
+        //13. only cars of the SEDAN type from a model newer than 2019 and the manufacturer's founding year less than 1919
+        List<Car> sedanCars = manufacturers.stream()
+                .filter(manufacturer -> manufacturer.yearOfCreation < 1919)
+                .flatMap(manufacturer -> manufacturer.models.stream())
+                .filter(model -> model.productionStartYear > 2019)
+                .flatMap(model -> model.cars.stream())
+                .collect(Collectors.toList());
+
+        if (cabrioCars.isEmpty()) {
+            System.out.println("No cars found with the specified conditions.");
+        } else {
+            System.out.println("Cars of the SEDAN type from a model newer than 2019 and the manufacturer's founding year less than 1919 " + sedanCars);
+        }
 
     }
 }
